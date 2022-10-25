a5a2e48f04ab025437f0b3928ec1152647e880a5
wangxinglong <wangxinglong@internal.xdrsec.com>
2022-10-25 11:17:41 +0800

    aes加密

---
+++ utils/common/encrypt.go
@@ -0,0 +1,82 @@
+package common
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+	"fmt"
+)
+
+// 加密过程：
+// 1、处理数据，对数据进行填充，采用PKCS7（当密钥长度不够时，缺几位补几个几）的方式。
+// 2、对数据进行加密，采用AES加密方法中CBC加密模式
+// 3、对得到的加密数据，进行base64加密，得到字符串
+// 解密过程相反
+
+// 16,24,32位字符串的话，分别对应AES-128，AES-192，AES-256 加密方法
+const AES_ENCRYPT_KEY = ""
+
+func AesEncrypt(orig string, key string) string {
+	// 转成字节数组
+	origData := []byte(orig)
+	k := []byte(key)
+
+	// 分组秘钥
+	block, err := aes.NewCipher(k)
+	if err != nil {
+		return fmt.Sprintf("key 长度必须 16/24/32长度: %s", err.Error())
+	}
+	// 获取秘钥块的长度
+	blockSize := block.BlockSize()
+	// 补全码
+	origData = PKCS7Padding(origData, blockSize)
+	// 加密模式
+	blockMode := cipher.NewCBCEncrypter(block, k[:blockSize])
+	// 创建数组
+	cryted := make([]byte, len(origData))
+	// 加密
+	blockMode.CryptBlocks(cryted, origData)
+	//使用RawURLEncoding 不要使用StdEncoding
+	//不要使用StdEncoding  放在url参数中回导致错误
+	return base64.RawURLEncoding.EncodeToString(cryted)
+}
+
+func AesDecrypt(cryted string, key string) string {
+	//使用RawURLEncoding 不要使用StdEncoding
+	//不要使用StdEncoding  放在url参数中回导致错误
+	crytedByte, _ := base64.RawURLEncoding.DecodeString(cryted)
+	k := []byte(key)
+
+	// 分组秘钥
+	block, err := aes.NewCipher(k)
+	if err != nil {
+		return fmt.Sprintf("key 长度必须 16/24/32长度: %s", err.Error())
+	}
+	// 获取秘钥块的长度
+	blockSize := block.BlockSize()
+	// 加密模式
+	blockMode := cipher.NewCBCDecrypter(block, k[:blockSize])
+	// 创建数组
+	orig := make([]byte, len(crytedByte))
+	// 解密
+	blockMode.CryptBlocks(orig, crytedByte)
+	// 去补全码
+	orig = PKCS7UnPadding(orig)
+
+	return string(orig)
+}
+
+//补码
+func PKCS7Padding(ciphertext []byte, blocksize int) []byte {
+	padding := blocksize - len(ciphertext)%blocksize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+//去码
+func PKCS7UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
